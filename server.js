0dd0f7ccb3419ee81fb97daa14f8cf0d9f6f6752
Sachin Krishnamurthy <sachinkrishnamurthy@mac.mynetworksettings.com>
2024-12-23 18:40:47 -0500

    Initial express setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode in PORT ${PORT}`));
